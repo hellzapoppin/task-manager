309b31cd097b777122fb784ccd30bb7de42735c5
José Leonardo <joseleonaordo85@gnail.com>
2025-03-05 19:10:42 -0300

    feat: add InputLabel component

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -6,6 +6,7 @@ import { CSSTransition } from 'react-transition-group';
 
 import Button from './Button';
 import Input from './input';
+import InputLabel from './InputLabel';
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
   const nodeRef = useRef(null);
@@ -37,7 +38,18 @@ const AddTaskDialog = ({ isOpen, handleClose }) => {
                   label="Titulo"
                   placeholder="Insira o título da tarefa"
                 />
-                <Input id="time" label="Horário" placeholder="Horário" />
+                <div className="flex flex-col gap-1 text-left">
+                  <InputLabel htmlFor="time">Horário</InputLabel>
+                  <select
+                    name="time"
+                    id="time"
+                    className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
+                  >
+                    <option value="morning">Manhã</option>
+                    <option value="afternoon">Tarde</option>
+                    <option value="evening">Noite</option>
+                  </select>
+                </div>
                 <Input
                   id="description"
                   label="Descrição"
---
+++ src/components/InputLabel.jsx
@@ -0,0 +1,12 @@
+const InputLabel = (props) => {
+  return (
+    <label
+      className="text-left text-sm font-semibold text-[#35383E]"
+      {...props}
+    >
+      {props.children}
+    </label>
+  );
+};
+
+export default InputLabel;
