9b2e3f946461bb6802926c98b8924e09880af49f
José Leonardo <joseleonaordo85@gnail.com>
2025-03-05 17:56:15 -0300

    feat: add size and secondary varient to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,33 @@
-const Button = ({ children, variant = 'primary', ...rest }) => {
+const Button = ({
+  children,
+  variant = 'primary',
+  size = 'small',
+  className,
+  ...rest
+}) => {
   const getVariantClasses = () => {
     if (variant === 'primary') {
       return 'bg-[#00ADB5] text-white';
+    }
+    if (variant === 'secondary') {
+      return 'bg-[#EEE] text-[#35383E]';
     }
     if (variant === 'ghost') {
       return 'bg-transparent text-[#818181]';
     }
   };
+
+  const getSizeClasses = () => {
+    if (size === 'small') {
+      return 'py-1 text-xs';
+    }
+    if (size === 'large') {
+      return 'py-2 text-sm';
+    }
+  };
   return (
     <button
-      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-60 ${getVariantClasses()}`}
+      className={`flex items-center justify-center gap-2 rounded-md px-3 font-semibold transition hover:opacity-60 ${getVariantClasses()} ${getSizeClasses()} ${className}`}
       {...rest}
     >
       {children}
