17d53f5861da8241f131970ba41b13f8adcc15cb
José Leonardo <joseleonaordo85@gnail.com>
2025-03-07 17:11:58 -0300

    feat: ensure tasks are featched from API

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
 import {
@@ -15,8 +15,20 @@ import TaskItem from './TaskItem';
 import TaskSeparator from './TasksSeparator';
 
 const Tasks = () => {
-  const [tasks, setTasks] = useState(TASKS);
+  const [tasks, setTasks] = useState([]);
   const [AddTaskDialogisOpen, setAddTaskDialogisOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch('http://localhost:3000/tasks', {
+        method: 'GET',
+      });
+      const tasks = await response.json();
+      setTasks(tasks);
+    };
+
+    fetchTasks();
+  }, []);
 
   const morningTasks = tasks.filter((task) => task.time === 'morning');
   const afternoonTasks = tasks.filter((task) => task.time === 'afternoon');
