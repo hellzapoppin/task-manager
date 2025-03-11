9297ae773b4924ec60106afdfc121dee19fe6dea
José Leonardo <joseleonaordo85@gnail.com>
2025-03-11 14:33:42 -0300

    feat: add initial TaskDetailsPage without logic

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,7 @@
 import AddIcon from './add.svg?react';
+import ArrowLeft from './arrow-left.svg?react';
 import CheckIcon from './check.svg?react';
+import ChevronRight from './chevron-right.svg?react';
 import CloudSunIcon from './cloud-sun.svg?react';
 import DetailsIcon from './details.svg?react';
 import HomeIcon from './home.svg?react';
@@ -11,7 +13,9 @@ import TrashIcon from './trash.svg?react';
 
 export {
   AddIcon,
+  ArrowLeft,
   CheckIcon,
+  ChevronRight,
   CloudSunIcon,
   DetailsIcon,
   HomeIcon,
--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { ArrowLeft, ChevronRight, TrashIcon } from '../assets/icons';
+import Button from '../components/Button';
+import Input from '../components/input';
+import InputLabel from '../components/InputLabel';
+import Sidebar from '../components/Sidebar';
+import TimeSelect from '../components/TimeSelect';
 
 const TaskDetailsPage = () => {
   const { taskId } = useParams();
   const [task, setTask] = useState([]);
+  const navigate = useNavigate();
+  const handleBackClick = () => {
+    navigate(-1);
+  };
   useEffect(() => {
     const fetchTask = async () => {
       const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
@@ -17,9 +28,61 @@ const TaskDetailsPage = () => {
     fetchTask();
   }, [taskId]);
   return (
-    <div>
-      <p>{task.title}</p>
-      <p>{task.description}</p>
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full space-y-6 px-8 py-16">
+        <div className="flex w-full justify-between">
+          <div>
+            <button
+              onClick={handleBackClick}
+              className="mb-3 flex h-8 w-8 items-center justify-center rounded-full bg-brand-primary"
+            >
+              <ArrowLeft />
+            </button>
+            <div className="flex items-center gap-1 text-xs">
+              <span
+                className="cursor-pointer text-brand-text-gray"
+                onClick={handleBackClick}
+              >
+                Minhas tarefas
+              </span>
+              <ChevronRight className="text-brand-text-gray" />
+              <span className="font-semibold text-brand-primary">
+                {task?.title}
+              </span>
+            </div>
+            <h1 className="mt-2 text-xl font-semibold">{task?.title}</h1>
+          </div>
+
+          <Button className="h-fit self-end" color="danger">
+            <TrashIcon />
+            Deletar tarefa
+          </Button>
+        </div>
+
+        <div className="space-y-6 rounded-xl bg-brand-white p-6">
+          <div>
+            <Input id="title" label="Nome" value={task?.title}></Input>
+          </div>
+          <div>
+            <TimeSelect value={task?.time} />
+          </div>
+          <div>
+            <Input
+              id="description"
+              label="Descrição"
+              value={task?.description}
+            ></Input>
+          </div>
+        </div>
+
+        <div className="flex w-full justify-end gap-3">
+          <Button size="large" color="secondary">
+            Cancelar
+          </Button>
+          <Button size="large">Salvar</Button>
+        </div>
+      </div>
     </div>
   );
 };
